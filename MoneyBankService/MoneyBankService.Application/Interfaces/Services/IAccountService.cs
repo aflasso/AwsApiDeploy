026f3989afde40be8f49e00add1f97ed50a68306
aflasso <andresfelipe.lasso@gmail.com>
2025-05-16 21:03:03 -0500

    service implementation

--- MoneyBankService/MoneyBankService.Application/Interfaces/Services/IAccountService.cs
+++ MoneyBankService/MoneyBankService.Application/Interfaces/Services/IAccountService.cs
@@ -7,8 +7,7 @@ public interface IAccountService
     Task<Account> CreateAccountAsync(Account account);
     Task<List<Account>> GetAccountsByAccountNumberAsync(string accountNumber);
     Task<Account> GetAccountByIdAsync(int accountId);
-    Task<Account> UpdateAccountAsync(int accountId, Account newAccount);
     Task DeleteAccountAsync(int accountId);
-    Task<bool> TryWithdrawAsync(int accountId, Transaction transaction);
-    Task<bool> TryDepositAsync(int accountId, Transaction transaction);
+    Task WithdrawAsync(int accountId, Transaction transaction);
+    Task DepositAsync(int accountId, Transaction transaction);
 }
